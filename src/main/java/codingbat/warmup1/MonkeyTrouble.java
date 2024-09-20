a61f62551549cc064344f7b04ca5afee75c1a400
Korlan <koramuka@gmail.com>
2024-09-20 14:08:46 +0600

    StringTimes

---
+++ src/main/java/codingbat/warmup1/MonkeyTrouble.java
@@ -0,0 +1,9 @@
+package codingbat.warmup1;
+
+public class MonkeyTrouble {
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if ((aSmile || bSmile) && (!aSmile || !bSmile)) {
+            return false;
+        } return true;
+    }
+}
---
+++ src/main/java/codingbat/warmup2/FrontTimes.java
@@ -0,0 +1,15 @@
+package codingbat.warmup2;
+
+public class FrontTimes {
+    public String frontTimes(String str, int n) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            if (str.length() >= 3) {
+                result.append(str.substring(0, 3));
+            } else {
+                result.append(str);
+            }
+        }
+        return result.toString();
+    }
+}
